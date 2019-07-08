4863b63c0a6148eca409f0d3faca503bf539e19f
Agustin Valdes <42925417+dasgrossfuchs@users.noreply.github.com>
2019-07-08 10:03:35 -0700

    First 2 proyects done while i had nthing ot do at work
    plz no be mad uwu theyre bad i was just bored and couldnt get any games to play at the office

---
+++ DiceMachineFull/DiceMachine/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace DiceMachine
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\chimes.wav");  
+            simpleSound.Play();  
+         */
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = Properties.Resources.start;           
+        }
+        
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"roll.wav");
+            simpleSound.Play();
+            for (int i = 0; i < 30; i++)
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(1, 7);
+                Thread.Sleep(100);
+                Roll(randomNumber);
+            }
+            
+                pictureBox1.Hide();
+                Thread.Sleep(150);
+                pictureBox1.Show();
+            
+        }
+        private void Roll(int x)
+        {
+            switch (x)
+            {
+                case 1: pictureBox1.Image = Properties.Resources.dice1; break;
+                case 2: pictureBox1.Image = Properties.Resources.dice2; break;
+                case 3: pictureBox1.Image = Properties.Resources.dice3; break;
+                case 4: pictureBox1.Image = Properties.Resources.dice4; break;
+                case 5: pictureBox1.Image = Properties.Resources.dice5; break;
+                case 6: pictureBox1.Image = Properties.Resources.dice6; break;
+                default:
+                    break;
+            }
+            pictureBox1.Update();
+        }
+    }
+}
---
+++ SpeedTestFull/Event Test/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Event_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+
+            const string tempfile = "tempfile.tmp";
+            System.Net.WebClient webClient = new System.Net.WebClient();
+
+           
+            label1.Text ="Descagando....";
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+            webClient.DownloadFile("http://dl.google.com/googletalk/googletalk-setup.exe", tempfile);
+            sw.Stop();
+
+            FileInfo fileInfo = new FileInfo(tempfile);
+            long speed = fileInfo.Length / sw.Elapsed.Milliseconds*1000;
+
+            //Console.WriteLine("Download duration: {0}", sw.Elapsed);
+            //Console.WriteLine("File size: {0}", fileInfo.Length.ToString("N0"));
+            //Console.WriteLine("Speed: {0} bps ", speed.ToString("N0"));
+            string finb = speed.ToString("N0")+" bps";
+            string fink= (speed/1024).ToString("N0") + " Kbps";
+            string finm = (speed / 1024/1024).ToString("N0") + " Mbps";
+            string fing = (speed / 1024/1024/1024).ToString("N0") + " Gbps";
+            label1.Text = finb;
+            label2.Text = fink;
+            label3.Text = finm;
+            label4.Text = fing;
+            label5.Text = fileInfo.Length.ToString("N0") + " bits descargados";
+            fileInfo.Delete();
+            label2.Show();
+            label3.Show();
+            label4.Show();
+            label5.Show();
+            //Console.WriteLine("Press any key to continue...");
+            //Console.ReadLine();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Iniciando...";
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+            label5.Hide();
+        }
+    }
+}
